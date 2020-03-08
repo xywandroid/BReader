e14609f74931145973f27d59848a3c58867b22ba
xingyan_wei <xingyan_wei@163.com>
2020-03-08 20:37:07 +0800

    提交修改布局文件

---
+++ app/src/main/java/com/coressoft/breader/ui/fragment/FragmentDonate.java
@@ -0,0 +1,34 @@
+package com.coressoft.breader.ui.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.coressoft.breader.R;
+import com.coressoft.breader.base.BaseFragment;
+
+public class FragmentDonate extends BaseFragment {
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initData(View view) {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_donate;
+    }
+}
